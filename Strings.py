e14c2d4e389b994a9ef7f37a99b89cbe666c005f
Deepak-Sharma28 <dk1999sharma1999@gmail.com>
2021-04-15 21:48:59 +0530

    added string problems solutions

--- Strings.py
+++ Strings.py
@@ -1,5 +1,7 @@
 #checking if a string is a anagram or not
 
+#method 1
+
 # main = "abc"
 # to_compared = "bca"
 # counter = 0
@@ -12,3 +14,49 @@
 #     print("nope")
 
 
+#method 2
+
+# main = "abc"
+# to_compared = "baa"
+# for i in range(len(main)):
+#     if main[i] in to_compared:
+#         if i == len(main)-1 and len(main) == len(to_compared):
+#             print("anagram string")
+#     else:
+#         print("Not a anagram string")
+
+
+#for reversing a string
+
+# user = "abcd"
+# for i in range(len(user)//2):
+#     user = list(user)
+#     user[i],user[(len(user)-1)-i] = user[(len(user)-1)-i],user[i]
+# print(*user)
+
+
+#program for checking palidrome sting
+
+
+# String = "aba"
+# temp = ""
+# for i in range(-1,-len(String)-1,-1):
+#     temp+=String[i]
+# if String == temp:
+#     print("Palindrome")
+# else:
+#     print("Nope")
+
+
+#for checking duplicates in a string
+
+# main = "abcdadc"
+# temp = ""
+# for i in range(1,len(main)):
+#     if main[i-1] in main[i:]:
+#         if main[i-1] not in temp:
+#             temp+=main[i-1]
+# if len(temp)!= 0:
+#     print(temp)
+# else:
+#     print("no duplicates")
