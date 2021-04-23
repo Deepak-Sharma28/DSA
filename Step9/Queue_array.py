d3753280a906ee4b2774806140abae04d31061d6
Deepak-Sharma28 <dk1999sharma1999@gmail.com>
2021-04-23 17:44:07 +0530

    added code of queue using array

---
+++ Step9/Queue_array.py
@@ -0,0 +1,29 @@
+queue = []
+
+def enqueue(element):
+    queue.insert(0,element)
+    return "inserted"
+    
+def dequeue():
+    return queue.pop(),"has removed from queue"
+
+def size():
+    return len(queue)
+
+def Print():
+    print(queue)
+    return "is queue"
+
+def isEmpty():
+    if len(queue) == 0:
+        return True
+    else:
+        return False
+def peek():
+    
+    return queue[-1]
+
+enqueue(0)
+enqueue(2)
+print(dequeue())
+print(size())
