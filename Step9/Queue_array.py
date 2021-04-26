58f404d740eff8ee93ea810cb4597d4c6f7675ab
Deepak-Sharma28 <dk1999sharma1999@gmail.com>
2021-04-26 15:02:46 +0530

    queue using stacks

--- Step9/Queue_array.py
+++ Step9/Queue_array.py
@@ -1,29 +1,29 @@
-queue = []
+# queue = []
 
-def enqueue(element):
-    queue.insert(0,element)
-    return "inserted"
+# def enqueue(element):
+#     queue.insert(0,element)
+#     return "inserted"
     
-def dequeue():
-    return queue.pop(),"has removed from queue"
+# def dequeue():
+#     return queue.pop(),"has removed from queue"
 
-def size():
-    return len(queue)
+# def size():
+#     return len(queue)
 
-def Print():
-    print(queue)
-    return "is queue"
+# def Print():
+#     print(queue)
+#     return "is queue"
 
-def isEmpty():
-    if len(queue) == 0:
-        return True
-    else:
-        return False
-def peek():
-    
-    return queue[-1]
+# def isEmpty():
+#     if len(queue) == 0:
+#         return True
+#     else:
+#         return False
+# def peek():
 
-enqueue(0)
-enqueue(2)
-print(dequeue())
-print(size())
+#     return queue[-1]
+
+# enqueue(0)
+# enqueue(2)
+# print(dequeue())
+# print(size())
---
+++ Step9/queue_stack.py
@@ -0,0 +1,22 @@
+# s1 = []
+# s2 = []
+
+# def enqueue(s1,element):
+#     s1.append(element)
+#     return s1
+
+# def dequeue(s1,s2,length):
+#     for i in range(length):
+#         s2.append(s1.pop())
+#     s2.pop()
+#     for j in range(len(s2)):
+#         s1.append(s2.pop())
+#     return s1
+# print(enqueue(s1,5))
+# print(enqueue(s1,6))
+# print(enqueue(s1,7))
+# print(s1)
+# print(dequeue(s1,s2,len(s1)))
+
+
+
