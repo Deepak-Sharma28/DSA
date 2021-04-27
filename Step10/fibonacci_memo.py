ac5d187482e296f1b1edc0f0b73bf29bf92794f5
Deepak-Sharma28 <dk1999sharma1999@gmail.com>
2021-04-27 16:19:53 +0530

    fib series with memo

---
+++ Step10/fibonacci_memo.py
@@ -0,0 +1,16 @@
+# memo = {}
+# def fib(n):
+#     if n == 2:
+#         return 1
+#     elif n == 1 :
+#         return 0
+#     elif n in memo:
+#         return memo[n]
+#     else:
+#        value = fib(n-1)+fib(n-2)
+#        memo[n] = value
+#        return value
+# n = int(input())       
+# for i in range(1,n+1):
+#     print(fib(i))
+
