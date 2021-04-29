092347574218c3f4c376657bcfe683cf2fa62f3a
Deepak-Sharma28 <dk1999sharma1999@gmail.com>
2021-04-29 18:24:39 +0530

    made some changes in step11 create_bt.py

--- Step11/create_bt.py
+++ Step11/create_bt.py
@@ -3,20 +3,26 @@ class Node:
         self.left = None
         self.right = None
         self.data = data
-    def insert(self,data):
+    def insertleft(self,data):
         if self.data:
             if self.left is None:
                 self.left = Node(data)
             else:
                 self.left.insert(data)
+        else:
+            self.data = data
+
+    def insertright(self,data):
+        if self.data:
             if self.right is None:
                 self.right = Node(data)
             else:
-                self.right.insert(data)
+                self.right.insertright(data)
         else:
             self.data = data
+
 root = Node(28)
-root.insert(15)
-root.insert(65)
-root.insert(60)
+root.insertleft(15)
+root.insertright(65)
+root.insertright(60)
 # print(root.self)
